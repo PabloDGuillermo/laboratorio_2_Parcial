3d8df9f898c039a41bbaa62cbbeb48c300af19b2
Pablo Guillermo <pablodanielguillermo@gmail.com>
2024-05-14 18:50:58 -0300

    ultimo commit

--- Guillermo.Pablo.23/ClassLibrary1/obj/Debug/net6.0/ClassLibrary1.AssemblyInfo.cs
+++ Guillermo.Pablo.23/ClassLibrary1/obj/Debug/net6.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c0a7eba0ddca1508a458265ed373640ad27f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d086d5eb70cec780d2b5cb50a68fbf0f2a00efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
